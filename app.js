90158d07bd32021efa3da11fe910a6e500326944
Divino Junio <c926978@pmmg.mg.gov.br>
2019-11-06 08:29:38 -0300

    Paralisado na aula 21 - instalando o mysql

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
-
-
-app.get('/', function(req, res) {
-	res.render("home/index");
-});
-
-app.get('/formulario_inclusao_noticia', function(req, res) {
-	res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res) {
-	res.render("noticias/noticias");
-});
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
 
 
 app.listen(PORT, function() {
